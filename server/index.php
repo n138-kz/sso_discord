ccc3791f14325cd4583532a1c9706c75f2d2a14c
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2025-01-29 05:50:24 +0900

    Update index.php

--- server/index.php
+++ server/index.php
@@ -1,2 +1,12 @@
 <?php
+date_default_timezone_set('Asia/Tokyo');
+header('Content-Type: application/json; charset=UTF-8');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Disposition, Content-Type, Content-Length, Accept-Encoding, Origin, Accept, Access-Control-Allow-Headers, Authorization, X-Requested-With, X-Token');
+header('Access-Control-Expose-Headers: *');
+
+header('Server: Hidden');
+header('X-Powered-By: Hidden');
+
 echo json_encode($_REQUEST);
