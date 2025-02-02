b1baff9bb5b01a9731a9bcbe67eec24cfa91ed98
Komiya Yu <komiya.yu@n138.jp>
2025-02-02 18:00:59 +0900

    DEBUG Export

--- server/index.php
+++ server/index.php
@@ -199,6 +199,21 @@ if($config['external']['discord']['webhook']['notice']['active']){
 		$webhook=$webhook->exec_curl();
 		if($webhook[0]!==null||$webhook[1]!==null){ error_log(json_encode($webhook)); }
 
+		if(isset($i)&&is_array($i)){
+			foreach($i as $k => $v){
+				$webhook=new discord();
+				$webhook->set_endpoint($config['external']['discord']['webhook']['notice']['endpoint']);
+				$webhook->set_value('username', 'Bot-WebHook_'.$result['result']['d_user']['username']);
+				$webhook->set_value('avatar_url', $result['result']['d_user']['avatar_url']);
+				$webhook->set_value('content', 'DEBUG Export');
+				$tmp=tempnam(sys_get_temp_dir(), 'php_'.hash('crc32',time()));
+				file_put_contents($tmp,json_encode($v,JSON_PRETTY_PRINT|JSON_INVALID_UTF8_IGNORE),LOCK_EX);
+				$tmp=new \CURLFile($tmp, 'application/json', 'tmp'.time().'.json');
+				$webhook->set_file($tmp);
+				$webhook=$webhook->exec_curl();
+			}
+		}
+
 	}catch(\Throwable $e){
 		error_log('Fetal: discord-webhook error: This was caught: '.$e->getMessage());
 	}catch(\Exception $e){
