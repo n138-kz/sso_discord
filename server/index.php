db69f646636fd841557e241a81e4411a19db40c7
Komiya Yu <komiya.yu@n138.jp>
2025-02-02 16:47:54 +0900

    投げるファイル

--- server/index.php
+++ server/index.php
@@ -186,6 +186,10 @@ if($config['external']['discord']['webhook']['notice']['active']){
 		$webhook->set_value('username', 'Bot-WebHook_'.$result['result']['d_user']['username']);
 		$webhook->set_value('avatar_url', $result['result']['d_user']['avatar_url']);
 		$webhook->set_value('content', '```json'.PHP_EOL.json_encode($result['result']).PHP_EOL.'```');
+		$tmp=tempnam(sys_get_temp_dir(), 'php_'.hash('crc32',time()));
+		file_put_contents($tmp,json_encode($result,JSON_PRETTY_PRINT|JSON_INVALID_UTF8_IGNORE),LOCK_EX);
+		$tmp=new \CURLFile($tmp, 'application/json', 'tmp'.time().'.json');
+		$webhook->set_file($tmp);
 		$embeds=[
 			[
 				'title'=>'Debug LOG discord-webhook',
