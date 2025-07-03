f5aff9745a214738c172a8912896a1e9a420dd27
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2025-07-04 02:12:17 +0900

    $curl_res http.response.code

--- server/token.php
+++ server/token.php
@@ -586,6 +586,14 @@ curl_setopt($curl_req, CURLOPT_FOLLOWLOCATION, TRUE);
 $curl_res=curl_exec($curl_req);
 $curl_res=json_decode($curl_res, TRUE);
 $curl_res_info=curl_getinfo($curl_req, CURLINFO_RESPONSE_CODE);
+$curl_res = (is_null($curl_res))?[]:$curl_res;
+$curl_res = array_merge([
+	'http'=>[
+		'response'=>[
+			'code'=>$curl_res_info,
+		]
+	]
+], $curl_res);
 error_log('['.__LINE__.'] '.json_encode($curl_res));
 $result['result']['directmessage_channel'] = array_merge($result['result']['directmessage_channel'], $curl_res);
 $result['result']['directmessage_channel']['api_http_response_code'] = $curl_res_info;
