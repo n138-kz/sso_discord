ec55d5a20db3096bf0abb90be5163f013a30fbe1
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2025-07-19 19:29:45 +0900

    CREATE VIEW sso_discord_discordme_view

--- server/create-table_discordme.sql
+++ server/create-table_discordme.sql
@@ -23,3 +23,30 @@ ALTER TABLE IF EXISTS sso_discord_discordme OWNER to webapp;
 ALTER TABLE IF EXISTS sso_discord_discordme
   ADD CONSTRAINT sso_discord_discordme_premium_type_fkey FOREIGN KEY (premium_type)
   REFERENCES discord_premium_type (id) MATCH SIMPLE;
+DROP VIEW IF EXISTS sso_discord_discordme_view;
+CREATE OR REPLACE VIEW sso_discord_discordme_view
+  AS
+  SELECT
+    to_timestamp(trunc(sso_discord_discordme."timestamp")) as timestamp,
+    sso_discord_discordme.userid,
+    sso_discord_discordme.username,
+    sso_discord_discordme.global_name,
+    sso_discord_discordme.avatar,
+    sso_discord_discordme.discriminator,
+    sso_discord_discordme.public_flags,
+    sso_discord_discordme.flags,
+    sso_discord_discordme.banner,
+    sso_discord_discordme.accent_color,
+    sso_discord_discordme.avatar_decoration_data,
+    sso_discord_discordme.collectibles,
+    sso_discord_discordme.banner_color,
+    sso_discord_discordme.clan,
+    sso_discord_discordme.primary_guild,
+    sso_discord_discordme.locale,
+    discord_premium_type.description as premium_type
+  FROM sso_discord_discordme
+  INNER JOIN discord_premium_type
+	ON
+    sso_discord_discordme.premium_type = discord_premium_type.id
+  ORDER BY
+    sso_discord_discordme."timestamp" DESC;
