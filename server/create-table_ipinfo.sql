793ad8e37f323b0cf1bcabbc60f98348382ca870
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2025-07-19 23:19:19 +0900

    VIEW sso_discord_ipinfo_view

--- server/create-table_ipinfo.sql
+++ server/create-table_ipinfo.sql
@@ -12,3 +12,18 @@ CREATE TABLE IF NOT EXISTS sso_discord_ipinfo (
   CONSTRAINT sso_discord_ipinfo_pkey PRIMARY KEY (ip)
 );
 ALTER TABLE IF EXISTS sso_discord_ipinfo OWNER to webapp;
+CREATE OR REPLACE VIEW sso_discord_ipinfo_view
+  AS
+  SELECT
+    to_timestamp(trunc(sso_discord_ipinfo."timestamp")) as timestamp,
+    sso_discord_ipinfo.ip,
+    sso_discord_ipinfo.asn,
+    sso_discord_ipinfo.as_name,
+    sso_discord_ipinfo.as_domain,
+    sso_discord_ipinfo.country_code,
+    sso_discord_ipinfo.country,
+    sso_discord_ipinfo.continent_code,
+    sso_discord_ipinfo.continent
+  FROM sso_discord_ipinfo
+  ORDER BY
+    sso_discord_ipinfo."timestamp" DESC;
